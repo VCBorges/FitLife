135ae3363e35bdb0f1eb3876622fb7c8251b1650
VCBorges <vinicius.costa.borges98@gmail.com>
2024-11-01 16:59:57 -0300

    chore: add toast notification after update the profile

--- static/js/pages/profile.js
+++ static/js/pages/profile.js
@@ -5,5 +5,12 @@ const PROFILE_FORM_ID = "profile-form-id";
 const PROFILE_FORM = document.getElementById(PROFILE_FORM_ID);
 
 PROFILE_FORM.addEventListener("submit", function (event) {
-  handleFormSubmit({ event });
+  handleFormSubmit({
+    event,
+    onSuccess: async (data) => {
+      const toast = document.getElementById("update-profile-toast-id");
+      const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toast);
+      toastBootstrap.show();
+    },
+  });
 });
