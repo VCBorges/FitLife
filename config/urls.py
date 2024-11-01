90f915715b91513d080295948c051bbcedcd4cff
VCBorges <vinicius.costa.borges98@gmail.com>
2024-10-31 21:27:52 -0300

    fix: remove brower reload from production

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,17 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('__reload__/', include('django_browser_reload.urls')),
     path('admin/', admin.site.urls),
     path('account/', include('apps.users.urls')),
     path('', include('apps.gym.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns.extend(
+        [
+            path('__reload__/', include('django_browser_reload.urls')),
+        ]
+    )
